7aebb9efd0ff43de8e658df0ec738e32fe8dbbb3
Víctor Enrique Montes Tovar <DANIELCASTELAO\vmontestovar@nautilus19.ad.danielcastelao.org>
2019-11-15 14:13:36 +0100

    Initial commit

---
+++ src/Clase.kt
@@ -0,0 +1,14 @@
+fun main(){
+    println("Hello world!")
+    println(sum(1,b= 3))
+println(printSum(a = 1,b = 4))
+}
+fun sum(a: Int, b: Int): Int {
+    return a + b
+}
+fun printSum(a: Int, b: Int): Unit {
+    println("sum of $a and $b is ${a + b}")
+
+
+    fun sum(a: Int, b: Int) = a + b}
+
